2ecc69fe5ce6e8cd84b065348441380cd4766461
Jillian Meehan <jillian@lickability.net>
2023-12-26 11:29:18 -0500

    adds some sample layouts & partials to get started

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
 module.exports = (config) => {
+  // set directories to pass through to the dist folder
+  config.addPassthroughCopy("./src/images/");
   return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     dir: {
       input: "src",
       output: "dist",
