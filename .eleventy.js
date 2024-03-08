87dec860e048aff1eb3fbe24f92adc34362316b7
jillian g. meehan <35230656+jilliangmeehan@users.noreply.github.com>
2024-03-07 23:55:40 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = (config) => {
   // set directories to pass through to the dist folder
   config.addPassthroughCopy("./src/images/");
+  config.addPassthroughCopy("./src/posts/assets");
   config.addPassthroughCopy("./src/favicon/");
   return {
     markdownTemplateEngine: "njk",
