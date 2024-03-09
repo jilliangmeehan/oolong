1c0e6f8482c87daad35529a4383579103841cc77
Jillian Meehan <jillian@lickability.net>
2024-03-09 16:20:15 -0500

    someday i'll understand how to do this

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = (config) => {
   // set directories to pass through to the dist folder
   config.addPassthroughCopy("./src/images/");
   config.addPassthroughCopy("./src/favicon/");
+  config.addPassthroughCopy("**/photos/*.jpg");
   return {
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
